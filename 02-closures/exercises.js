aa2b7aa6caafd03b999ab67387a61ead18627309
Sybil <vanessa.zgn@gmail.com>
2023-02-09 23:40:36 +0100

    Completed up to challenge 12

--- 02-closures/exercises.js
+++ 02-closures/exercises.js
@@ -7,7 +7,6 @@ function createFunction() {
 // const function1 = createFunction();
 // function1(); // => should console.log('hello');
 
-
 // CHALLENGE 2
 function createFunctionPrinter(input) {
   return () => console.log(input)
@@ -138,63 +137,85 @@ function rollCall(names) {
 
 // CHALLENGE 8
 function saveOutput(func, magicWord) {
-
+ const objectOfValues = {}
+  return (input) => {
+   if (input === magicWord) {
+     return objectOfValues
+   } else {
+     objectOfValues[input] = func(input)
+     return func(input)
+   }
+  }
 }
 
 // /*** Uncomment these to check your work! ***/
-// const multiplyBy2 = function(num) { return num * 2; };
-// const multBy2AndLog = saveOutput(multiplyBy2, 'boo');
-// console.log(multBy2AndLog(2)); // => should log 4
-// console.log(multBy2AndLog(9)); // => should log 18
-// console.log(multBy2AndLog('boo')); // => should log { 2: 4, 9: 18 }
+ // const multiplyBy2 = function(num) { return num * 2; };
+ // const multBy2AndLog = saveOutput(multiplyBy2, 'boo');
+ // console.log(multBy2AndLog(2)); // => should log 4
+ // console.log(multBy2AndLog(9)); // => should log 18
+ // console.log(multBy2AndLog('boo')); // => should log { 2: 4, 9: 18 }
 
 
 // CHALLENGE 9
 function cycleIterator(array) {
-
+  let timesInvoked = -1
+  return ()=> {
+    timesInvoked++
+    return array[timesInvoked % array.length]
+  }
 }
 
 // /*** Uncomment these to check your work! ***/
-// const threeDayWeekend = ['Fri', 'Sat', 'Sun'];
-// const getDay = cycleIterator(threeDayWeekend);
-// console.log(getDay()); // => should log 'Fri'
-// console.log(getDay()); // => should log 'Sat'
-// console.log(getDay()); // => should log 'Sun'
-// console.log(getDay()); // => should log 'Fri'
-
+ // const threeDayWeekend = ['Fri', 'Sat', 'Sun'];
+ // const getDay = cycleIterator(threeDayWeekend);
+ // console.log(getDay()); // => should log 'Fri'
+ // console.log(getDay()); // => should log 'Sat'
+ // console.log(getDay()); // => should log 'Sun'
+ // console.log(getDay()); // => should log 'Fri'
 
 // CHALLENGE 10
 function defineFirstArg(func, arg) {
-
+  return (input) => {
+    return func(arg, input)
+  }
 }
 
 // /*** Uncomment these to check your work! ***/
-// const subtract = function(big, small) { return big - small; };
-// const subFrom20 = defineFirstArg(subtract, 20);
-// console.log(subFrom20(5)); // => should log 15
+ // const subtract = function(big, small) { return big - small; };
+ // const subFrom20 = defineFirstArg(subtract, 20);
+ // console.log(subFrom20(5)); // => should log 15
 
 
 // CHALLENGE 11
 function dateStamp(func) {
-
+ const now = Date.now()
+  return (...args)=> {
+   return { date: now, output: func(args)}
+  }
 }
 
 // /*** Uncomment these to check your work! ***/
-// const stampedMultBy2 = dateStamp(n => n * 2);
-// console.log(stampedMultBy2(4)); // => should log { date: (today's date), output: 8 }
-// console.log(stampedMultBy2(6)); // => should log { date: (today's date), output: 12 }
+ // const stampedMultBy2 = dateStamp(n => n * 2);
+ // console.log(stampedMultBy2(4)); // => should log { date: (today's date), output: 8 }
+ // console.log(stampedMultBy2(6)); // => should log { date: (today's date), output: 12 }
 
 
 // CHALLENGE 12
 function censor() {
-
+  const objectStrings = {}
+  return (firstStr, secondStr)=> {
+    if (!secondStr) {
+      return Object.entries(objectStrings).reduce((acc, [key, value])=> acc.replace(key, value), firstStr)
+    }
+    objectStrings[firstStr] = secondStr
+  }
 }
 
 // /*** Uncomment these to check your work! ***/
-// const changeScene = censor();
-// changeScene('dogs', 'cats');
-// changeScene('quick', 'slow');
-// console.log(changeScene('The quick, brown fox jumps over the lazy dogs.')); // => should log 'The slow, brown fox jumps over the lazy cats.'
+ // const changeScene = censor();
+ // changeScene('dogs', 'cats');
+ // changeScene('quick', 'slow');
+ // console.log(changeScene('The quick, brown fox jumps over the lazy dogs.')); // => should log 'The slow, brown fox jumps over the lazy cats.'
 
 
 // CHALLENGE 13
